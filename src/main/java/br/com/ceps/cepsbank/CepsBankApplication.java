c7797fa74844328e1b77d905bce1ef41428eaaed
caparking <cepassos1@gmail.com>
2023-11-03 12:36:56 -0300

    Initial structure

---
+++ src/main/java/br/com/ceps/cepsbank/CepsBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.ceps.cepsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepsBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepsBankApplication.class, args);
+	}
+
+}
